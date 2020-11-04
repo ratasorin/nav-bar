71c4b84198adbaa01647975b789478b446b2c7ee
rata sorin <ratasorin0@gmail.com>
2020-11-04 09:57:59 +0200

    first subbmit

---
+++ main.js
@@ -0,0 +1,39 @@
+const bars = document.querySelectorAll(".fa.fa-minus");
+document
+  .querySelector(".menu-bars")
+  .addEventListener("mouseenter", function () {
+    let char = 0;
+    let timer = setInterval(onTick, 150);
+
+    function onTick() {
+      bars[char].classList.add("active");
+      char++;
+      if (char === bars.length) complete();
+      return;
+    }
+    function complete() {
+      clearInterval(timer);
+      timer = null;
+    }
+    document.querySelector(".menu-bars").classList.add("active");
+    document.querySelector(".nav-info").classList.add("active");
+  });
+document
+  .querySelector(".menu-bars")
+  .addEventListener("mouseleave", function () {
+    let char = 0;
+    let timer = setInterval(onTick, 90);
+
+    function onTick() {
+      bars[char].classList.remove("active");
+      char++;
+      if (char === bars.length) complete();
+      return;
+    }
+    function complete() {
+      clearInterval(timer);
+      timer = null;
+    }
+    document.querySelector(".menu-bars").classList.remove("active");
+    document.querySelector(".nav-info").classList.remove("active");
+  });
